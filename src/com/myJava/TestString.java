ec42b2389c1b2259bedb6436e38620dab46136b7
gavin <gavinaa123aah@163.com>
2019-07-26 00:47:51 +0800

    equals 和 ==比较

--- src/com/myJava/TestString.java
+++ src/com/myJava/TestString.java
@@ -23,7 +23,32 @@ package com.myJava;
  * 第(1)种方式是在常量池创建一个字符串常量，此时如果再创建一个String s3 = "hello",会去常量池寻找相同常量，不会开辟一个新的地址空间。
  * 第(2)种方式是堆中创建地址空间存放，此时如果再创建一个String s4 = new String("hello")会开辟一个新的地址空间存放。
  *
+ *
  * String类重写了equals()方法，比较的是字符串的内容（将字符串拆程char数组，比较每一个字符内容）
+ *
+ * 源码如下：
+ *  public boolean equals(Object anObject) {
+ *         if (this == anObject) {
+ *             return true;
+ *         }
+ *         if (anObject instanceof String) {
+ *             String anotherString = (String)anObject;
+ *             int n = value.length;
+ *             if (n == anotherString.value.length) {
+ *                 char v1[] = value;
+ *                 char v2[] = anotherString.value;
+ *                 int i = 0;
+ *                 while (n-- != 0) {
+ *                     if (v1[i] != v2[i])
+ *                         return false;
+ *                     i++;
+ *                 }
+ *                 return true;
+ *             }
+ *         }
+ *         return false;
+ *     }
+ *     
  * s1,s3内存地址相等，所以对于s1==s3或s1.equals(s3)返回的都是true
  * s2,s4内存地址不相等，内容相等。所以s2==s4返回false,s1==s3返回true
  *
